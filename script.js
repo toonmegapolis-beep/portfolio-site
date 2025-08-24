7ec8b4439670a34fea2c32cdd1bab2f712c4508e
toonmegapolis-beep <toonmegapolis@gmail.com>
2025-08-24 13:02:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: show a thank-you message when form is submitted
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Thanks for reaching out! I'll get back to you soon.");
+});
